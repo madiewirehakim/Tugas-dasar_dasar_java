4fe695029fc540335e1094f53f186d0d91b8339c
madiewirehakim <85764886+madiewirehakim@users.noreply.github.com>
2022-09-27 13:32:45 +0700

    Add files via upload

---
+++ projet_tugas/src/tugas.java
@@ -0,0 +1,29 @@
+public class tugas {
+    public static void main(String[] args){
+        //deklarasi variabel
+        String namadepan, namabelakang, alamat, jeniskelamin;
+        String tanggallahir;
+        int umur;
+        String tahun;
+
+        //pengisian variabel
+        namadepan="Madie Wire";
+        namabelakang="Hakim";
+        tanggallahir="03 Juni 2000";
+        alamat="Rebuk II";
+        jeniskelamin="Laki-Laki";
+        umur=20;
+        tahun="2000 Juni, senin";
+
+
+        System.out.println("\t----------------------------");
+        System.out.println("\t\t\tBIODATA SAYA");
+        System.out.println("\tNama Depan\t\t: "+namadepan);
+        System.out.println("\tNama Belakang\t: "+namabelakang);
+        System.out.println("\ttanggallahir\t: "+tanggallahir);
+        System.out.println("\tAlamat\t\t\t: "+alamat);
+        System.out.println("\tJenis Kelamin\t: "+jeniskelamin);
+        System.out.println("\tUmur\t\t\t: "+umur);
+        System.out.println("\ttahun\t\t\t: "+tahun);
+    }
+}
